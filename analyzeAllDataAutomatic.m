c817e067ec9761201ef25c30c7748a06ca89e211
Ookenfooken <40247563+Ookenfooken@users.noreply.github.com>
2018-09-28 12:06:33 -0700

    added automatic analysis

---
+++ analyzeAllDataAutomatic.m
@@ -0,0 +1,65 @@
+%% Script to analyze all collected data automatically and write trial 
+% info into mat files that will be saved per subject
+% this script is the autmatic equivalent to viewEyeData.m and requires 
+% analyzeTrialAutomatic.mat
+% if you update sampling rate, saccade threshold or similar, please
+% also update in viewEyeData.m 
+
+% history
+% 2012-2016     JF created analyzeAllPlayersAllTrials.m 
+% 28-09-2018    JF renamed script and commented to make the script more accecable 
+%				for future VPOM students
+% for questions email jolande.fooken@rwth-aachen.de 
+
+% define general experimental settings here
+sampleRate = 1000;
+screenSizeY = 38.8;
+screenResX = 1280;
+screenResY = 1024;
+distance = 46.25;
+distanceZ = 17;     %distance from finger to screen in initial position
+saccadeThreshold = 50; %threshold for saccade sensitivity
+microSaccadeThreshold = 6;
+
+analysisPath = pwd;
+dataPath = fullfile(pwd,'..','data\');
+resultPath = fullfile(pwd,'results\');
+
+%% create list of all subjects
+folderNames = dir(dataPath);
+currentSubject = {};
+
+
+%% Loop over all subjects
+
+for i = 3:length(folderNames)
+    currentSubject{i-2} = folderNames(i).name;
+    
+    currentFolder = [dataPath currentSubject{i-2}];
+    cd(currentFolder);
+    
+    numTrials = length(dir('*edf'));
+    eyeFiles = dir('*.asc');
+	% load mat file containing experimental infor
+	load('parameters.mat');
+    % go into analysis folder
+    cd(analysisPath)
+    % now we are inside the subject's folder and loop over trials
+    %% analyze for each trial
+    for currentTrial = 1:numTrials[results.trial] = analyzeTrialAutomatic(eyeFiles, currentTrial, currentSubject{i-2}, analysisPath, dataPath, parameters);
+        analysisResults(:,currentTrial) = results;
+    end
+    
+    cd(resultPath)
+    save(currentSubject{i-2}, 'analysisResults')
+    cd(analysisPath)
+    
+    clear analysisResults
+    clear results
+    clear eyeFiles
+    clear ascfiles
+    clear numTrials
+    clear currentFolder
+    clear trialList
+
+end
---
+++ analyzeTrialAutomatic.m
@@ -0,0 +1,45 @@
+%% This function is the magic script that analyzes all your eye data
+% it is basically the equivalent to analyzeTrial.m; just a function so that
+% we can run it automatically. same requirements as analyzeTrial.m
+
+% history
+% 07-2012       JE created analyzeTrialautomatic.m
+% 2012-2018     JF added stuff to and edited analyzeTrialAutomatic.m
+% 28-09-2018    JF commented to make the script more accecable for future 
+%               VPOM students
+% for questions email jolande.fooken@rwth-aachen.de
+
+function [trial, pursuit] = analyzeTrialAutomatic(eyeFiles, currentTrial, currentSubject, analysisPath, dataPath, parameters)
+    %% Eye Data
+    %  eye data have been converted in readEDF
+    %  first step: read in converted eye data
+    ascFile = eyeFiles(currentTrial,1).name;
+    eyeData = readEyeData(ascFile, dataPath, currentSubject, analysisPath, syncIdx);
+    eyeData = processEyeData(eyeData); % equivalent to socscalexy
+
+
+    %% find saccades
+	threshold = evalin('base', 'saccadeThreshold');
+	[saccades.X.onsets, saccades.X.offsets] = findSaccades(onset, offset, trial.eyeDX_filt, trial.eyeDDX_filt, threshold, stimulusVelocityX);
+	[saccades.Y.onsets, saccades.Y.offsets] = findSaccades(onset, offset, trial.eyeDY_filt, trial.eyeDDY_filt, threshold, stimulusVelocityY);
+
+    %% analyze saccades
+	[trial] = analyzeSaccades(trial, saccades);
+	clear saccades;
+    
+	%% OPTIONAL: find and analyze pursuit
+	% pursuit = findPursuit(trial); 
+	% % remove saccades
+	% trial = removeSaccades(trial);
+	% % analyze pursuit
+	% pursuit = analyzePursuit(trial, pursuit);
+
+	%% OPTIONAL: find micro saccades
+	% % remove saccades
+	% trial = removeSaccades(trial);
+	% m_threshold = evalin('base', 'microSaccadeThreshold');
+	% [saccades.X.onsets, saccades.X.offsets] = findSaccades(onset, offset, trial.DX_noSac, trial.DDX_noSac, m_threshold, 0);
+	% [saccades.Y.onsets, saccades.Y.offsets] = findSaccades(onset, offset, trial.DY_noSac, trial.DDY_noSac, m_threshold, 0);
+	% % analyze micro-saccades
+	% [trial] = analyzeMicroSaccades(trial, saccades);
+end
